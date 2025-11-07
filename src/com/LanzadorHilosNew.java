e84de7ebe49a5f62f8f7357b3b1d3807bbda4a13
jlorbor2710 <lorite.borrego.jose.tomas@iescamas.es>
2025-11-07 20:08:41 +0100

    Estado runnable

--- src/com/LanzadorHilosNew.java
+++ src/com/LanzadorHilosNew.java
@@ -1,8 +1,10 @@
 package com;
 
 public class LanzadorHilosNew {
-	public static void main(String[] args) {
-		Thread h = new Thread(new Hilo("nuevo"));
+	public static void main(String[] args) throws InterruptedException {
+		Thread h = new Thread(new Hilo("runneable"));
+		h.start();
 		System.out.println(h.getName() + "-->" + h.getState());
+		Thread.sleep(50);
 	}
 }
