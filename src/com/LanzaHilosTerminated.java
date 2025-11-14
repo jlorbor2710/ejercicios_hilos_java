601f115150ce0fe177128b17e4fdcf766bf3900a
root <lorite.borrego.jose.tomas@iescamas.es>
2025-11-14 19:56:36 +0100

    Estado terminado

--- src/com/LanzaHilosTerminated.java
+++ src/com/LanzaHilosTerminated.java
@@ -1,10 +1,11 @@
 package com;
 
-public class LanzadorHilosNew {
-	public static void main(String[] args) throws InterruptedException {
-		Thread h = new Thread(new Hilo("runneable"));
-		h.start();
-		System.out.println(h.getName() + "-->" + h.getState());
-		Thread.sleep(50);
+public class LanzaHilosTerminated {
+	public static void main(String [] args) throws InterruptedException {
+		Thread t = new Thread(new Hilo("finalizado"));
+		t.start();
+		t.join();
+		System.out.println(t.getName() + " ---> " + t.getState());
 	}
+
 }
